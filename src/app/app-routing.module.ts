49fda2818785c245e4dee7715847568c95b5e4e4
Fábio Sala <imfabiosala@gmail.com>
2022-12-10 18:40:51 -0300

    [FEAT] Config. app routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './vendor/list/list.component';
+import { FormComponent } from './vendor/form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListComponent },
+  { path: 'vendor/list', component: ListComponent },
+  { path: 'vendor/new', component: FormComponent },
+  { path: 'vendor/:type/:id/', component: FormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
