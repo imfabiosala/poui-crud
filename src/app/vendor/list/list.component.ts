b920ad9577820776592a3c9c3c6c3e8bce37547a
Fábio Sala <imfabiosala@gmail.com>
2022-12-11 09:25:46 -0300

    [FEAT] Add. vendors table to list component.

--- src/app/vendor/list/list.component.ts
+++ src/app/vendor/list/list.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PoNotificationService } from '@po-ui/ng-components';
+import { ListService } from './list.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
+
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  vendorList: Array<any> = new Array();
+  columnTable: Array<any> = new Array();
+
+  constructor(private listService: ListService, private router: Router, private poNotification: PoNotificationService) { }
 
   ngOnInit(): void {
+    this.updateVendorList();
+    this.columnTable = this.listService.getColumns();
+  }
+
+  updateVendorList(): void {
+    this.listService.getVendorList().subscribe(response => {
+      this.vendorList = response.items;
+    });
   }
 
 }
