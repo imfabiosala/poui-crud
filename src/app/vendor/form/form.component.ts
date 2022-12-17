01612344319216ed47af1fe3600ae29abe185102
Fábio Sala <imfabiosala@gmail.com>
2022-12-17 12:03:03 -0300

    [FEAT] Add. dynamic city select form.

--- src/app/vendor/form/form.component.ts
+++ src/app/vendor/form/form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { PoDynamicFormField, PoNotificationService } from '@po-ui/ng-components';
+import { PoDynamicFormField, PoNotificationService, PoDynamicFormFieldChanged, PoDynamicFormValidation } from '@po-ui/ng-components';
 import { Vendor } from '../model/vendor.model';
 import { FormService } from './form.service';
 import { first } from 'rxjs';
@@ -14,6 +14,8 @@ import { first } from 'rxjs';
 export class FormComponent implements OnInit {
 
   vendor: Vendor = new Vendor();
+
+  validateFields: Array<string> = ['stateId'];
 
   vendorValues = {
     type: 2,
@@ -113,46 +115,16 @@ export class FormComponent implements OnInit {
       property: 'stateId',
       label: 'Estado',
       options: [
-        { label: 'Acre', value: 'AC' },
-        { label: 'Alagoas', value: 'AL' },
-        { label: 'Amapá', value: 'AP' },
-        { label: 'Amazonas', value: 'AM' },
-        { label: 'Bahia', value: 'BA' },
-        { label: 'Ceará', value: 'CE' },
-        { label: 'Distrito Federal', value: 'DF' },
         { label: 'Espírito Santo', value: 'ES' },
-        { label: 'Goiás', value: 'GO' },
-        { label: 'Maranhão', value: 'MA' },
-        { label: 'Mato Grosso', value: 'MT' },
-        { label: 'Mato Grosso do Sul', value: 'MS' },
-        { label: 'Minas Gerais', value: 'MG' },
-        { label: 'Pará', value: 'PA' },
-        { label: 'Paraíba', value: 'PB' },
-        { label: 'Paraná', value: 'PR' },
-        { label: 'Pernambuco', value: 'PE' },
-        { label: 'Piauí', value: 'PI' },
+        { label: 'Minas Gerais',   value: 'MG' },
         { label: 'Rio de Janeiro', value: 'RJ' },
-        { label: 'Rio Grande do Norte', value: 'RN' },
-        { label: 'Rio Grande do Sul', value: 'RS' },
-        { label: 'Rondônia', value: 'RO' },
-        { label: 'Roraima', value: 'RR' },
-        { label: 'Santa Catarina', value: 'SC' },
-        { label: 'São Paulo', value: 'SP' },
-        { label: 'Sergipe', value: 'SE' },
-        { label: 'Tocantins', value: 'TO' },
-        { label: 'Estrangeiro', value: 'EX' }
+        { label: 'São Paulo',      value: 'SP' }
       ]
     },
     {
       property: 'cityCode',
       label: 'Cidade',
-      options: [
-        { label: 'Colatina', value: '01506' },
-        { label: 'Adolfo', value: '00204' },
-        { label: 'São José do Rio Preto', value: '49805' },
-        { label: 'José Bonifácio', value: '25706' },
-        { label: 'Joinville', value: '09102' }
-      ]
+      disabled: true
     }
   ];
 
@@ -168,7 +140,7 @@ export class FormComponent implements OnInit {
       this.vendorValues.zipCode               = vendor.address.zipCode;
       this.vendorValues.address               = vendor.address.address;
       this.vendorValues.stateId               = vendor.address.state.stateId;
-      this.vendorValues.cityCode              = vendor.address.city.cityCode;
+      this.vendorValues.cityCode              = vendor.address.city.cityDescription;
     });
   };
 
@@ -185,6 +157,18 @@ export class FormComponent implements OnInit {
     this.vendor.address.state.stateId          = this.vendorValues.stateId;
     this.vendor.address.city.cityCode          = this.vendorValues.cityCode;
   };
+
+  onChangeFields(changedValue: PoDynamicFormFieldChanged): PoDynamicFormValidation {
+    return {
+      fields: [
+        {
+          property: 'cityCode',
+          options: this.formService.getCities(changedValue.value.stateId),
+          disabled: false
+        }
+      ]
+    };
+  }
 
   insertVendor(): void {
     this.getForm();
--- src/app/vendor/form/form.service.ts
+++ src/app/vendor/form/form.service.ts
@@ -13,6 +13,44 @@ export class FormService {
 
   constructor(private http: HttpClient) { }
 
+  getCities(stateId: string) {
+    switch (stateId) {
+      case 'ES': {
+        return [
+          { label: 'Colatina', value: '01506' },
+          { label: 'Linhares', value: '03205' },
+          { label: 'Serra',    value: '05002' },
+          { label: 'Vitória',  value: '05309' }
+        ];
+      }
+      case 'MG': {
+        return [
+          { label: 'Belo Horizonte', value: '06200' },
+          { label: 'Juiz de Fora',   value: '36702' },
+          { label: 'Nanuque',        value: '44300' },
+          { label: 'Uberlândia',     value: '70206' }
+        ];
+      }
+      case 'RJ': {
+        return [
+          { label: 'Duque de Caxias', value: '01702' },
+          { label: 'Niterói',         value: '03302' },
+          { label: 'Rio de Janeiro',  value: '04557' },
+          { label: 'Volta Redonda',   value: '06305' }
+        ];
+      }
+      case 'SP': {
+        return [
+          { label: 'Campinas',              value: '09502' },
+          { label: 'Guarulhos',             value: '18800' },
+          { label: 'São Bernardo do Campo', value: '48708' },
+          { label: 'São Paulo',             value: '50308' }
+        ];
+      }
+    }
+    return [];
+  }
+
   postVendor(formData: string) {
     return this.http.post(this.apiUrl, formData)
   }
